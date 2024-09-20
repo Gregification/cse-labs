b519cfd5e3b6f4db43bce5437d1a251699aa5eee
gregclacker <yongboone1@gmail.com>
2024-09-19 20:30:57 -0500

    complete cse3442 lab3

---
+++ cse3442/lab3a/Debug/src/subdir_vars.mk
@@ -0,0 +1,52 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+S_SRCS += \
+../src/lab3a_GeorgeBoone_s.s 
+
+C_SRCS += \
+../src/clock.c \
+../src/lab3a_GeorgeBoone.c \
+../src/lab3b_GeorgeBoone.c 
+
+S_DEPS += \
+./src/lab3a_GeorgeBoone_s.d 
+
+C_DEPS += \
+./src/clock.d \
+./src/lab3a_GeorgeBoone.d \
+./src/lab3b_GeorgeBoone.d 
+
+OBJS += \
+./src/clock.obj \
+./src/lab3a_GeorgeBoone.obj \
+./src/lab3a_GeorgeBoone_s.obj \
+./src/lab3b_GeorgeBoone.obj 
+
+OBJS__QUOTED += \
+"src\clock.obj" \
+"src\lab3a_GeorgeBoone.obj" \
+"src\lab3a_GeorgeBoone_s.obj" \
+"src\lab3b_GeorgeBoone.obj" 
+
+C_DEPS__QUOTED += \
+"src\clock.d" \
+"src\lab3a_GeorgeBoone.d" \
+"src\lab3b_GeorgeBoone.d" 
+
+S_DEPS__QUOTED += \
+"src\lab3a_GeorgeBoone_s.d" 
+
+C_SRCS__QUOTED += \
+"../src/clock.c" \
+"../src/lab3a_GeorgeBoone.c" \
+"../src/lab3b_GeorgeBoone.c" 
+
+S_SRCS__QUOTED += \
+"../src/lab3a_GeorgeBoone_s.s" 
+
+
---
+++ cse3442/lab3a/src/lab3a_GeorgeBoone_s.s
@@ -0,0 +1,30 @@
+; UTA cse3442 lab3a
+; George Boone
+; 9/17/2024
+
+; assuming 40Mhz clock. 123 pipeline rule.
+
+    .def wait3Seconds
+
+
+.thumb
+
+.const
+W3S_ITTR_N          .field   29999998
+
+.text
+
+; blocks for 3 seconds
+; function wastes [120M - 1] clock cycles, next command called after this runs on the 120Mth clock since this funciton was called
+wait3Seconds:                                                               ; time evaluation
+                LDR     R0, W3S_ITTR_N     ; itterating 'n' times           ; 1
+                NOP                                                         ; 1
+                NOP                                                         ; 1
+                NOP                                                         ; 1
+                NOP                                                         ; 1
+itterate:       SUB     R0, R0, #1                                          ; n
+                CBZ     R0, break                                           ; n + 3
+                B       itterate                                            ; 2n
+break:          BX      LR                                                  ; 2
+
+.endm
