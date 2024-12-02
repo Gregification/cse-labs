3073886d22529672301004387b1d650ff4437ac5
gregclacker <yongboone1@gmail.com>
2024-12-02 14:33:27 -0600

    complete lab7
    was done last week, forgot to push

--- cse3442/lab7/src/lab7_George_Boone.c
+++ cse3442/lab7/src/lab7_George_Boone.c
@@ -22,14 +22,14 @@
 #include <inttypes.h>
 #include "../tm4c123gh6pm.h"
 
-#include "uart0.h"
 #include "MCP23S08_support.h"
 
 #define _BV(X)  (1 << X)
-#define SCK_M     _BV(2)
-#define SS_M      _BV(3)
-#define DI_M      _BV(4)
-#define DO_M      _BV(5)
+#define SCK_M       _BV(2)
+#define SS_M        _BV(3)
+#define DI_M        _BV(4)
+#define DO_M        _BV(5)
+#define INT_M       _BV(6)
 
 #define SS_BB       (*((volatile uint32_t *)(3 * 4 + 0x42000000 + (0x400043FC - 0x40000000) * 32)))
 #define SS_ON       0
@@ -40,7 +40,7 @@ uint8_t SSI0_SPI_master_transfer(uint8_t);
 void    SSI0_SPI_master_transfer_arr(uint8_t * tx, uint8_t * rx, size_t len);
 
 uint8_t SSI0_SPI_MCP23S08_READ(uint8_t addr);
-void    SSI0_SPI_MCP23S08_WRITE(uint8_t addr, uint8_t val);
+uint8_t SSI0_SPI_MCP23S08_WRITE(uint8_t addr, uint8_t val);
 
 bool BTN_PRESSED = false;
 
@@ -49,11 +49,6 @@ int main(void)
     //init system clock to 40MHz
     SYSCTL_RCC_R = SYSCTL_RCC_XTAL_16MHZ | SYSCTL_RCC_OSCSRC_MAIN | SYSCTL_RCC_USESYSDIV | (4 << SYSCTL_RCC_SYSDIV_S);
 
-    //----------------init uart--------------------------
-    initUart0();
-    setUart0BaudRate(115200, 40e6);
-    putsUart0("lab7 start\n\r");
-
 
     //---------------init ssi0 as spi--------------------
     SSI0_SPI_init_master();
@@ -61,17 +56,15 @@ int main(void)
     _delay_cycles(10e6);
 
     //------------MCP to lab requirements----------------
-//    {
-//        uint8_t tx[1] = {0x4};
-//        SSI0_SPI_master_transfer_arr(tx, tx, sizeof(tx));
-//    }
 
     /*GPIO
      * input    : 7
      * output   : 4, 5
      */
+    uint8_t v;
+    v = SSI0_SPI_MCP23S08_READ(MCP23S08_ADDR_IODIR);
     SSI0_SPI_MCP23S08_WRITE(MCP23S08_ADDR_IODIR, _BV(7));
-    uint8_t v = SSI0_SPI_MCP23S08_READ(MCP23S08_ADDR_IODIR);
+    v = SSI0_SPI_MCP23S08_READ(MCP23S08_ADDR_IODIR);
     // general configuration
     SSI0_SPI_MCP23S08_WRITE(MCP23S08_ADDR_INTCON,   0x06);      // interrupt line active high, enable hardware addressing
 
@@ -85,14 +78,14 @@ int main(void)
     _delay_cycles(3);
 
     // GPIO setup
-    GPIO_PORTA_DIR_R    &=  ~_BV(6);            // as input
-    GPIO_PORTA_DEN_R    |=  _BV(6);             // as digital
+    GPIO_PORTA_DIR_R    &=  ~INT_M;            // as input
+    GPIO_PORTA_DEN_R    |=  INT_M;             // as digital
 
     // set up interrupt
-    GPIO_PORTA_IM_R     |=  _BV(6);             // disable port A interrupts
-    GPIO_PORTA_IS_R     |=  _BV(6);             // level sensitive
-    GPIO_PORTA_IEV_R    |=  _BV(6);             // active high
-    GPIO_PORTA_IM_R     |=  _BV(6);             // enable port A interrupts
+    GPIO_PORTA_IM_R     |=  INT_M;             // disable port A interrupts
+    GPIO_PORTA_IS_R     |=  INT_M;             // level sensitive
+    GPIO_PORTA_IEV_R    |=  INT_M;             // active high
+    GPIO_PORTA_IM_R     |=  INT_M;             // enable port A interrupts
 
 
     //---------------main logic--------------------------
@@ -170,62 +163,46 @@ void SSI0_SPI_master_transfer_arr(uint8_t * tx, uint8_t * rx, size_t len){
      *  this is configured to transmit 9 bits along with extra logic to convert it to 8 bits
      */
 
-    uint8_t former_cr0 = SSI0_CR0_R;
-    // SSI0 to 9 bit mode
-    SSI0_CR1_R  &=  ~SSI_CR1_SSE;       // disable to safely configure
-    SSI0_CR0_R  &=  ~(SSI_CR0_DSS_M);   // clear bit mode
-    SSI0_CR0_R  |=  SSI_CR0_DSS_9;      // 9 bit mode
-    SSI0_CR1_R  |=  SSI_CR1_SSE;        // enable
-
     SS_BB = SS_ON;
 
     // write one read one approach
     size_t i;
     for(i = 0; i < len; i++){
-        uint16_t tx_b = tx[i] << 1;
-        SSI0_DR_R = ((uint16_t)tx[i]) << 1;
+        SSI0_DR_R = tx[i];
 
         while(SSI0_SR_R & SSI_SR_BSY);
 
         if(rx)
-            rx[i] = SSI0_DR_R >> 1;
-        else
-            SSI0_DR_R;//read to clear buffer
+            rx[i] = SSI0_DR_R;
+        else while(SSI0_SR_R & SSI_SR_RNE)    // clear rx buffer
+            SSI0_DR_R;
     }
 
     SS_BB = SS_OFF;
-
-    // revert local SSI0 CR0 changes
-    SSI0_CR1_R  &=  ~SSI_CR1_SSE;       // disable to safely configure
-    SSI0_CR0_R  =   former_cr0;
-    SSI0_CR1_R  |=  SSI_CR1_SSE;        // enable
 }
 
 uint8_t SSI0_SPI_MCP23S08_READ(uint8_t addr){
-    uint8_t txrx[2];
+    uint8_t txrx[3] = {
+            MCP23S08_OPC | MCP23S08_OPC_RW_M,
+            addr,
+            0x00
+        };
 
-    txrx[0] = MCP23S08_OPC;
-    txrx[1] = addr;
-    SSI0_SPI_master_transfer_arr(txrx, NULL, 2);
+    SSI0_SPI_master_transfer_arr(txrx, txrx, sizeof(txrx));
 
-    txrx[0] = MCP23S08_OPC | MCP23S08_OPC_RW_M;
-    txrx[1] = addr;
-    SSI0_SPI_master_transfer_arr(txrx, txrx, 2);
-
-    return txrx[1];
+    return txrx[2];
 }
 
-void SSI0_SPI_MCP23S08_WRITE(uint8_t addr, uint8_t val){
-    uint8_t tx[2];
+uint8_t SSI0_SPI_MCP23S08_WRITE(uint8_t addr, uint8_t val){
+    uint8_t txrx[3] = {
+                MCP23S08_OPC,
+                addr,
+                val
+            };
 
-    tx[0] = MCP23S08_OPC;
-    tx[1] = addr;
-    SSI0_SPI_master_transfer_arr(tx, NULL, 2);
+    SSI0_SPI_master_transfer_arr(txrx, txrx, sizeof(txrx));
 
-    tx[0] = MCP23S08_OPC;
-    tx[1] = val;
-    SSI0_SPI_master_transfer_arr(tx, NULL, 2);
-
+    return txrx[2];
 }
 
 void intr_MCP23S08(){
@@ -235,20 +212,10 @@ void intr_MCP23S08(){
     // clear interrupt flags on the MCP
     SSI0_SPI_MCP23S08_READ(MCP23S08_ADDR_INTCAP);
 
-    // no need to clear local interrupt flags as its level detected
-
-    putsUart0("mcp interrupt!  ");
-    {
-        char str[10];
-        snprintf(str, sizeof(str), "x%02x", flags);
-        putsUart0(str);
-    }
+    // clear local interrupt flags
+    GPIO_PORTA_ICR_R    |= INT_M;
 
     if(flags & _BV(7)){
         BTN_PRESSED = true;
-        putsUart0("   button pressed");
-    } else {
-        putsUart0("   not button pressed");
     }
-    putsUart0("\n\r");
 }
