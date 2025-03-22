5859b6d5e052c7201573c5a7b1133982f5479fb5
gregclacker <yongboone1@gmail.com>
2025-03-22 14:24:32 -0500

    updated adc dumper

--- homelab/mspm0l1306fiddle/test3/src/main.cpp
+++ homelab/mspm0l1306fiddle/test3/src/main.cpp
@@ -4,12 +4,15 @@
 
 #include <ti/devices/msp/msp.h>
 #include <ti/driverlib/driverlib.h>
+#include <cstdio> // snprintf
+#include <inttypes.h>// len safe prints
 
 #define BV(X) (1 << (X))
 
 void ex_gpio();
 void ex_uart();
 void ex_spi();
+void ex_adc_to_uart();
 
 /*
  * about data sheet pin <-> peripheral lookup
@@ -31,7 +34,8 @@ int main(void)
 
 //    ex_gpio();
 //    ex_uart();
-    ex_spi();
+    ex_spi(); // add bias resistor?
+    ex_adc_to_uart();
 
 }
 
@@ -172,20 +176,100 @@ void ex_spi(){
 
     uint8_t data[] = {1,2,3,4,5,6,7,8,9,BV(0),BV(1),BV(2),BV(3),BV(4),BV(5),BV(6),BV(7)};
 
-    // depending on the spi m/s mode, this either transmits or just fills the fifo
 
-    DL_SPI_transmitData8(SPI0, 1);
+    while(DL_SPI_isBusy(SPI0))
+            ;
+    for(uint32_t i = 0; i < sizeof(data);){
+        while(!DL_SPI_isTXFIFOEmpty(SPI0))
+            ;
+        // depending on the spi m/s mode, this either transmits or just fills the fifo
+        i += DL_SPI_fillTXFIFO8(SPI0, data + i, sizeof(data) - i);
+    }
 
-//    while(DL_SPI_isBusy(SPI0))
-//            ;
-//    for(uint32_t i = 0; i < sizeof(data);){
-//        while(!DL_SPI_isTXFIFOEmpty(SPI0))
-//            ;
-//        i += DL_SPI_fillTXFIFO8(SPI0, data + i, sizeof(data) - i);
-//    }
+}
 
-//    DL_SPI_enablePacking(spi)(spi, buffer, maxCount)
+void ex_adc_to_uart(){
+    DL_GPIO_reset(GPIOA);
+    DL_ADC12_reset(ADC0);
+    DL_UART_reset(UART0);
+    DL_GPIO_enablePower(GPIOA);
+    DL_ADC12_enablePower(ADC0);
+    DL_UART_enablePower(UART0);
+    delay_cycles(16);
 
-    // CS has to be turned off manually
-//    DL_SPI_togglePins(GPIOA, BV(6));
+    { // UART init
+        // init gpio alt function
+        //      - IOMUX values can be found on table 6-1. pg.8 of family documentation
+        DL_GPIO_initPeripheralInputFunction(IOMUX_PINCM::IOMUX_PINCM10, IOMUX_PINCM10_PF_UART0_RX);
+        DL_GPIO_initPeripheralOutputFunction(IOMUX_PINCM::IOMUX_PINCM9, IOMUX_PINCM9_PF_UART0_TX);
+
+        // init uart0
+        DL_UART_ClockConfig clkconf = {
+            .clockSel    = DL_UART_CLOCK::DL_UART_CLOCK_BUSCLK,
+            .divideRatio = DL_UART_CLOCK_DIVIDE_RATIO::DL_UART_CLOCK_DIVIDE_RATIO_1,     // do not divide clock source
+        };
+        DL_UART_Config config = {
+           .mode        = DL_UART_MODE::DL_UART_MODE_NORMAL,
+           .direction   = DL_UART_DIRECTION::DL_UART_DIRECTION_TX_RX,
+           .flowControl = DL_UART_FLOW_CONTROL::DL_UART_FLOW_CONTROL_NONE,
+           .parity      = DL_UART_PARITY::DL_UART_PARITY_NONE,
+           .wordLength  = DL_UART_WORD_LENGTH::DL_UART_WORD_LENGTH_8_BITS,
+           .stopBits    = DL_UART_STOP_BITS::DL_UART_STOP_BITS_ONE
+        };
+
+        DL_UART_setClockConfig(UART0, &clkconf);
+        DL_UART_init(UART0, &config);
+        DL_UART_setOversampling(UART0, DL_UART_OVERSAMPLING_RATE::DL_UART_OVERSAMPLING_RATE_16X);
+        DL_UART_setBaudRateDivisor(UART0, 208, 21);                 // 9600 baud @ 32Mhz & 16x sampling. see mcu.tech.19.2.3.4 for calculation. note a latch register must also be written to, DL handles that.
+
+        DL_UART_enableMajorityVoting(UART0);
+
+        // apparently the FIFOS have to be enabled separately, the FIFOS arn't necessary for operation
+        DL_UART_enableFIFOs(UART0);
+        DL_UART_setRXFIFOThreshold(UART0, DL_UART_RX_FIFO_LEVEL::DL_UART_RX_FIFO_LEVEL_1_2_FULL);
+        DL_UART_setTXFIFOThreshold(UART0, DL_UART_TX_FIFO_LEVEL::DL_UART_TX_FIFO_LEVEL_1_2_EMPTY);
+
+        DL_UART_enable(UART0);
+    }
+
+    {// ADC init
+        DL_ADC12_ClockConfig clkconf = {
+            .clockSel       = DL_ADC12_CLOCK_SYSOSC,
+            .freqRange      = DL_ADC12_CLOCK_FREQ_RANGE_24_TO_32,    // expected ADC clock input range
+            .divideRatio    = DL_ADC12_CLOCK_DIVIDE_1,
+        };
+
+        DL_ADC12_setClockConfig(ADC0, &clkconf);
+
+        //IMPORTANT : SEE 14.2.5 clocking requirements, certain clock speeds must ONLY be used with certain sources/input-channels, ... etc
+        DL_ADC12_configConversionMem(
+                ADC0,
+                DL_ADC12_MEM_IDX_0,
+                DL_ADC12_INPUT_CHAN_0,                  // a.k.a input pin
+                DL_ADC12_REFERENCE_VOLTAGE_VDDA,        // INTREF configurable 1.4V to 2.5V but limited clock speed, EXTERN uses VREF+- pins, VDD uses mcu supply voltage
+                DL_ADC12_SAMPLE_TIMER_SOURCE_SCOMP0,    // idk
+                DL_ADC12_AVERAGING_MODE_DISABLED,
+                DL_ADC12_BURN_OUT_SOURCE_DISABLED,      // ADC peripheral integrity checker
+                DL_ADC12_TRIGGER_MODE_AUTO_NEXT,        // idk
+                DL_ADC12_WINDOWS_COMP_MODE_DISABLED     // idk
+            );
+        DL_ADC12_enableConversions(ADC0);
+    }
+
+    while(true){
+        delay_cycles(5e6);
+
+        uint16_t val = DL_ADC12_getMemResult(ADC0, DL_ADC12_MEM_IDX_0);
+
+        char its[6];
+        snprintf(its, sizeof(its), "%" PRIu16, val);
+        char str[18];
+        snprintf(str, sizeof(str), "ADC:PA27:%s\n\r", its);
+
+        for(uint8_t i = 0; i < sizeof(str) && str[i] != '\0'; i++){
+            while(DL_UART_isBusy(UART0))
+                ;
+            DL_UART_transmitDataBlocking(UART0, str[i]);
+        }
+    }
 }
