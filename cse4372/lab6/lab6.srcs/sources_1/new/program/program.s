0778fd57cd498f3b4b6f6aa9c854e9969c0d1f3b
gregclacker <yongboone1@gmail.com>
2025-03-27 01:13:20 -0500

    lab6 rearranged pipeline

---
+++ cse4372/lab6/lab6.srcs/sources_1/new/program/program.s
@@ -0,0 +1,69 @@
+# the hex output needs to be moved to the parent folder afterwards
+# 1. assemble to elf : 		riscv32-unknown-elf-gcc -o program program.s -march=rv32i -mabi=ilp32 -nostdlib -Tcse4372_riscv.ld
+# 2. extract bin :			riscv32-unknown-elf-objcopy program -O binary program.bin
+# 3. convert to hex : 		hexdump -v -e '1/4 "%08x" "\n"' program.bin > ram.hex
+# dump assembly : 			riscv32-unknown-elf-objdump -dr program > program.asm_dump
+
+.globl	_start
+
+.text
+
+_start:
+	
+	addi t0, t0, 6
+
+	nop
+	nop
+	nop
+
+	or t0, t0, 3
+ 
+	nop
+	nop
+	nop
+
+	and t1, t1, 0
+
+	nop
+	nop
+	nop
+
+	or t1, t1, 5
+
+	nop
+	nop
+	nop
+
+	or t2, t1, t0
+
+	nop
+	nop
+	nop
+
+	slli t0, t0, 3
+
+	nop
+	nop
+	nop
+
+	add t3, t0, t1
+
+	nop
+	nop
+	nop
+
+	ebreak
+
+	nop
+	nop
+	nop
+
+	add t3, t0, t1
+
+	nop
+	nop
+	nop
+
+	ebreak
+
+.end
---
+++ cse4372/lab6/lab6.srcs/sources_1/new/rv32_id_top.sv
@@ -0,0 +1,92 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 03/16/2025 05:25:44 PM
+// Design Name: 
+// Module Name: rv32_id_top
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+`include "defines.vh"
+
+// instruciton decode
+module rv32_id_top(
+        // system clock and synchronous reset
+        input clk,
+        input reset,
+
+        // from if
+        input [31:0] pc_in,
+        input [31:0] iw_in,
+
+        // dataforwarding for hazard resolution
+        // df from ex
+        input [31:0] df_ex_wb_reg,
+        input df_ex_wb_enable,
+        // df from mem
+        // df from wb
+
+        // register interface
+        output reg [4:0] regif_rs1_reg,
+        output reg [4:0] regif_rs2_reg,
+        input [31:0] regif_rs1_data_in,
+        input [31:0] regif_rs2_data_in,
+
+        // to ex
+        output reg [31:0] pc_out,
+        output reg [31:0] iw_out,
+        output reg [4:0] wb_reg_out,
+        output reg wb_enable_out,
+        output reg [31:0] regif_rs1_data_out,
+        output reg [31:0] regif_rs2_data_out
+    );
+
+    wire [6:0] opcode;
+    assign opcode   = iw_in[6:0];
+
+    always_ff @ (posedge clk) begin
+        if(reset) begin
+            pc_out <= `PC_RESET;
+            iw_out <= `IW_RESET;
+
+            wb_reg_out      <= 0;
+            wb_enable_out   <= 0;
+
+            regif_rs1_reg <= 0;
+            regif_rs2_reg <= 0;
+        end else begin
+            pc_out <= pc_in;
+            iw_out <= iw_in;
+
+            wb_reg_out      <= iw_in[11:7];
+            wb_enable_out   <= 
+                        opcode == 7'b0110011    // R type : orange
+                    ||  opcode == 7'b0000011    // I type subsection : dull red
+                    ||  opcode == 7'b0010011    // ^ : yellow
+                    ||  opcode == 7'b0110111    // U type : dull blue
+                    ||  opcode == 7'b0010111    // U type : dark green
+                ;
+
+            regif_rs1_reg <= iw_in[19:15];
+            regif_rs2_reg <= iw_in[24:20];
+        end
+    end
+
+    always_comb begin
+        regif_rs1_data_out = regif_rs1_data_in;
+        regif_rs2_data_out = regif_rs2_data_in;
+    end
+
+endmodule
