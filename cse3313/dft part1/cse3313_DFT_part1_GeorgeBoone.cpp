6a9f673abeec96fd718f77be28bbbe132de43393
gregclacker <yongboone1@gmail.com>
2025-04-20 15:55:57 -0500

    lab8

--- cse3313/dft part1/cse3313_DFT_part1_GeorgeBoone.cpp
+++ cse3313/dft part1/cse3313_DFT_part1_GeorgeBoone.cpp
@@ -4,10 +4,18 @@
  * 1002055713 
  * George Boone
  * 
- * build :              $ g++ -std=c++20 cse3313_DFT_part1_GeorgeBoone.cpp -o dft1.out
- * run w/ streaming:    $ ./dft1.out <dft/idft> <n-points> < file.csv
- * example 64 point dft: $ ./dft1.out dft 64 < file.csv
- * example 55 point idft: $ ./dft1.out idft 55 < file.csv
+ * 
+ * build :                  $ g++ -std=c++20 cse3313_DFT_part1_GeorgeBoone.cpp -o dft1.out
+ * run w/ streaming:        $ ./dft1.out <dft/idft> <n-points> < file.csv
+ * example 64 point dft:    $ ./dft1.out dft 64 < file.csv
+ * example 55 point idft:   $ ./dft1.out idft 55 < file.csv
+ * example output to clipboard:     $  ./dft1.out dft 64 < response.csv | xclip -selection clipboard
+ * 
+ * output example : [-1+2i,3+4i]
+ * """
+ *      -1,2
+ *      3,4
+ * """
  * 
  * DFT and IDFT done using the summation equation
  */
@@ -22,11 +30,12 @@
 
 using namespace std;
 
-typedef complex<long double> UNIT;
+typedef long double UNIT;
+typedef complex<UNIT> COMPLEX;
 
-vector<UNIT> parseInput(istream &input);
-vector<UNIT> dft(vector<UNIT> sequence, size_t n);
-vector<UNIT> idft(vector<UNIT> sequence, size_t n);
+vector<COMPLEX> parseInput(istream &input);
+vector<COMPLEX> dft(vector<COMPLEX> sequence, size_t n);
+vector<COMPLEX> idft(vector<COMPLEX> sequence, size_t n);
 
 int main(int argc, char *argv[]) {
     if (argc != 3 || (string(argv[1]) != "dft" && string(argv[1]) != "idft") || !stoul(argv[2])) {
@@ -38,7 +47,7 @@ int main(int argc, char *argv[]) {
     bool isDFT = string(argv[1]) == "dft";
     size_t n = stoul(argv[2]);
 
-    vector<UNIT> seq = parseInput(cin);
+    vector<COMPLEX> seq = parseInput(cin);
 
     if(isDFT)
         seq = dft(seq, n);
@@ -46,63 +55,63 @@ int main(int argc, char *argv[]) {
         seq = idft(seq, n);
 
     for (int i = 0; i < seq.size(); i++) {
-        cout << seq[i] << endl;
+        cout << seq[i].real() << "," << seq[i].imag() << endl;
     }
     return 0;
 }
 
-vector<UNIT> parseInput(istream &is){
-    vector<UNIT> arr;
+vector<COMPLEX> parseInput(istream &is){
+    vector<COMPLEX> arr;
 
     // parse complex input to vector
     // example input : 0.98,-0.89655-0.47921i,0.55137+0.82518i,-0.097637-0.99133i,
     for(string str; getline(is, str, ',');) {
-        UNIT temp;
+        COMPLEX temp;
 
-        size_t numend;
-        temp.real(stold(str, &numend));
         try{
+            size_t numend;
+            temp.real(stold(str, &numend));
             temp.imag(stold(str.substr(numend)));
-        } catch(const exception&){ }
+        } catch(const exception&){}
 
         arr.push_back(temp);
     }
     return arr;
 }
 
-vector<UNIT> dft(vector<UNIT> s, size_t n){
+vector<COMPLEX> dft(vector<COMPLEX> s, size_t n){
     // f[k] =   sum<j=0 to n-1>(f[j] * e^(-i2<pi>jk/n))                              // base equation
-    //      =>  sum<j=0 to n-1>(f[j] * (cos(-2<pi>jk/n) + i * sin(-2<pi>jk/n)) )    // expanded for computation
+    //      =>  sum<j=0 to n-1>(f[j] * (cos(-2<pi>jk/n) + i * sin(-2<pi>jk/n)) )     // expanded for computation
     // f[k] : frequency content
     // s[k] : sample sequence
     // expanded using : e^(ix) = cos(x) + i*sin(x)
     
-    vector<UNIT> f(n); // f[k]
+    vector<COMPLEX> f(n); // f[k]
 
     s.resize(n);// handles zero padding
 
-    for (int k = 0; k < n; k++) {
-        for (int j = 0; j < n; j++) {
-            f[k] += s[j] * UNIT(cos(-2 * M_PI * j * k / n), sin(-2 * M_PI * j * k / n));
+    for (UNIT k = 0; k < n; k++) {
+        for (UNIT j = 0; j < n; j++) {
+            f[k] += s[j] * COMPLEX(cos(-2 * M_PI * j * k / n), sin(-2 * M_PI * j * k / n));
         }
     }
     return f;
 }
 
-vector<UNIT> idft(vector<UNIT> f, size_t n){
+vector<COMPLEX> idft(vector<COMPLEX> f, size_t n){
     // s[k] =   sum<j=0 to n-1>(f[j] * e^(i2<pi>jk/n)) / n                              // base equation
     //      =>  sum<j=0 to n-1>(f[j] * (cos(2<pi>jk/n) + i * sin(2<pi>jk/n)) ) / n      // expanded for computation
     // s[k] : sample sequence
     // f[k] : frequency content
     // expanded using : e^(ix) = cos(x) + i*sin(x)
     
-    vector<UNIT> s(n); // s[k]
+    vector<COMPLEX> s(n); // s[k]
     
     f.resize(n);// handles zero padding
 
-    for (int k = 0; k < n; k++) {
-        for (int j = 0; j < n; j++) {
-            s[k] += f[j] * UNIT(cos(2 * M_PI * j * k / n), sin(2 * M_PI * j * k / n));
+    for (UNIT k = 0; k < n; k++) {
+        for (UNIT j = 0; j < n; j++) {
+            s[k] += f[j] * COMPLEX(cos(2 * M_PI * j * k / n), sin(2 * M_PI * j * k / n));
         }
 
         s[k] /= n;
