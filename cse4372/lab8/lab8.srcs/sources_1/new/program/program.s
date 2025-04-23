1304c45cbed6111b609165f2ece695f8cb45be7f
gregclacker <yongboone1@gmail.com>
2025-04-23 01:29:33 -0500

    o;;/

--- cse4372/lab8/lab8.srcs/sources_1/new/program/program.s
+++ cse4372/lab8/lab8.srcs/sources_1/new/program/program.s
@@ -12,106 +12,113 @@ _start:
 	nop
 
 # test memory R/W
-# 	li t6, 0xaabb
-# 	li t0, 0x0000002c
-# 	lw t1, 0(t0)
-# 	nop
-# 	nop
-# 	sw t6, 0(t0)
-# 	nop
-# 	nop
-# 	nop
-# 	nop
-# 	nop
-# 	nop
-# 	lw t1, 0(t0)
-# 	nop
-# 	nop
+	li t6, 0xaabb
+	li t0, 0x0000002c
+	lw t1, 0(t0)
+	nop
+	nop
+	sw t6, 0(t0)
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	lw t1, 0(t0)
+	nop
+	nop
 
-# 	lh t1, 0(t0)
-# 	nop
-# 	nop
+	lh t1, 0(t0)
+	nop
+	nop
 
-# 	lb t1, 0(t0)
-# 	nop
-# 	nop
+	lb t1, 0(t0)
+	nop
+	nop
 
-# 	lbu t1, 0(t0)
-# 	nop
-# 	nop
+	lbu t1, 0(t0)
+	nop
+	nop
 
 # # test io R/W
-# 	addi t3, x0, 0
-	li t0, 0x80000000  	# led address
-# 	# li t2, 0xFFFFFFFF  	# xor mask
-# 	li t4, 0x2faf08		# 50e6 loop
+	lw t1, 0(t0)
+	nop
+	nop
+
+	add t4, t1, x0
+	nop
+	li t0, 0x00000002
+	lh t1, 0(t0)
+	# add t2, t1, x0
+	# sh t3, 0(t2)
+	nop
+	nop
+	add t4, t1, x0
+	li t0, 0x00000003
+	lb t1, 0(t0)
+	# add t2, t3, x0
+	# lw t1, 0(t0)
+	nop
+	nop
+	add t4, t1, x0
+	add t4, t1, x0
+	add t4, t1, x0
+	add t2, x0, t1
+
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+
+
+	addi t0, x0, 1 
+	addi t0, x0, 2 
+	addi t0, x0, 3 
+	addi t0, x0, 4 
+	addi t0, x0, 5 
+	addi t0, x0, 6 
+	addi t0, x0, 7 
+	addi t0, x0, 8 
+	add  t1, x0, t0 	
+	add  t2, x0, t0 
+	add  t3, x0, t0 
+	add  t4, x0, t0
+
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+
+	addi t2, x0, 5 # number of itterations
+	addi t1, x0, 3 # some value to be modified
+	addi t0, x0, 0 # index
+itterate:
+	addi t0, t0, 1 # itterate
+	slli t1, t1, 1 # do something
+	bne t2, t0, itterate
 
 	addi t1, x0, 1
-
+	li t0, 0x80000000  	# led address
+	li t4, 0x80000004	# pb address
 ledloop:
 	addi t1, t1, 1
-	srli t2, t1, 15
+	srli t2, t1, 17
 	sw t2, 0(t0)
 	nop
 	nop
-	bne x0, t1, ledloop
+	lw t5, 0(t4)
+	nop
+	nop
+	andi t5, t5, 2
+	bne x0, t5, ledloop
 
-# 	lw t1, 0(t0)
-# 	nop
-# 	nop
-
-# 	add t4, t1, x0
-# 	nop
-# 	li t0, 0x00000002
-# 	lh t1, 0(t0)
-# 	# add t2, t1, x0
-# 	# sh t3, 0(t2)
-# 	nop
-# 	nop
-# 	add t4, t1, x0
-# 	li t0, 0x00000003
-# 	lb t1, 0(t0)
-# 	# add t2, t3, x0
-# 	# lw t1, 0(t0)
-# 	nop
-# 	nop
-# 	add t4, t1, x0
-# 	add t4, t1, x0
-# 	add t4, t1, x0
-# 	add t2, x0, t1
-
-# 	nop
-# 	nop
-# 	nop
-# 	nop
-# 	nop
-# 	nop
-
-
-# 	addi t0, x0, 1 
-# 	addi t0, x0, 2 
-# 	addi t0, x0, 3 
-# 	addi t0, x0, 4 
-# 	addi t0, x0, 5 
-# 	addi t0, x0, 6 
-# 	addi t0, x0, 7 
-# 	addi t0, x0, 8 
-# 	add  t1, x0, t0 	
-# 	add  t2, x0, t0 
-# 	add  t3, x0, t0 
-# 	add  t4, x0, t0
-
-# 	nop
-# 	nop
-# 	nop
-
-# 	addi t2, x0, 5 # number of itterations
-# 	addi t1, x0, 3 # some value to be modified
-# 	addi t0, x0, 0 # index
-# itterate:
-# 	addi t0, t0, 1 # itterate
-# 	slli t1, t1, 1 # do something
-# 	bne t2, t0, itterate
 
 	ebreak
 .end
