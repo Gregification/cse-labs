494ca39abe644181a8fa9b76d1009341d31d45d2
gregclacker <yongboone1@gmail.com>
2025-05-09 11:48:01 -0500

    compiler updated itself and seems to acutall compile things now

---
+++ homelab/tm4c129ncfiddle/BeeMS/src/main.cpp
@@ -0,0 +1,19 @@
+/*
+ * main.cpp
+ *
+ *  Created on: May 9, 2025
+ *      Author: turtl
+ */
+
+
+#include "FreeRTOS.h"
+#include "task.h"
+
+int main(){
+
+    vTaskStartScheduler();
+
+    while(1)
+        ;
+}
+
