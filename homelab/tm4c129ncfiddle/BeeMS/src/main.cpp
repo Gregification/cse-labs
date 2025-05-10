1d1477750592a6e69accff5800a5bd67f8a94ec7
gregclacker <yongboone1@gmail.com>
2025-05-10 16:55:26 -0500

    maybee working rots. missing interrupt handlers though

--- homelab/tm4c129ncfiddle/BeeMS/src/main.cpp
+++ homelab/tm4c129ncfiddle/BeeMS/src/main.cpp
@@ -5,15 +5,99 @@
  *      Author: turtl
  */
 
+#include "system.hpp"
+
+#include "inc/hw_sysctl.h"
+#include "driverlib/interrupt.h"
+#include "driverlib/rom.h"
+#include "driverlib/rom_map.h"
+#include "driverlib/sysctl.h"
 
 #include "FreeRTOS.h"
 #include "task.h"
 
 int main(){
+    /*---init hardware-------------------------------------------*/
+
+    System::CPU_FREQ = SysCtlClockFreqSet((SYSCTL_XTAL_25MHZ |
+        SYSCTL_OSC_MAIN | SYSCTL_USE_PLL |
+        SYSCTL_CFG_VCO_240), configCPU_CLOCK_HZ);
+
+    MAP_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
+    MAP_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOB);
+    MAP_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOC);
+    MAP_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOD);
+    MAP_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOE);
+    MAP_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
+    MAP_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOG);
+    MAP_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOH);
+    MAP_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOJ);
+    MAP_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOK);
+    MAP_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOL);
+    MAP_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOM);
+    MAP_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPION);
+    MAP_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOP);
+    MAP_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOQ);
 
     vTaskStartScheduler();
 
     while(1)
         ;
 }
+
+/*-----------------------------------------------------------*/
+
+void vApplicationMallocFailedHook( void )
+{
+    /* vApplicationMallocFailedHook() will only be called if
+    configUSE_MALLOC_FAILED_HOOK is set to 1 in FreeRTOSConfig.h.  It is a hook
+    function that will get called if a call to pvPortMalloc() fails.
+    pvPortMalloc() is called internally by the kernel whenever a task, queue,
+    timer or semaphore is created.  It is also called by various parts of the
+    demo application.  If heap_1.c or heap_2.c are used, then the size of the
+    heap available to pvPortMalloc() is defined by configTOTAL_HEAP_SIZE in
+    FreeRTOSConfig.h, and the xPortGetFreeHeapSize() API function can be used
+    to query the size of free heap space that remains (although it does not
+    provide information on how the remaining heap might be fragmented). */
+//    IntMasterDisable();
+    for( ;; );
+}
+/*-----------------------------------------------------------*/
+
+void vApplicationIdleHook( void )
+{
+    /* vApplicationIdleHook() will only be called if configUSE_IDLE_HOOK is set
+    to 1 in FreeRTOSConfig.h.  It will be called on each iteration of the idle
+    task.  It is essential that code added to this hook function never attempts
+    to block in any way (for example, call xQueueReceive() with a block time
+    specified, or call vTaskDelay()).  If the application makes use of the
+    vTaskDelete() API function (as this demo application does) then it is also
+    important that vApplicationIdleHook() is permitted to return to its calling
+    function, because it is the responsibility of the idle task to clean up
+    memory allocated by the kernel to any task that has since been deleted. */
+}
+/*-----------------------------------------------------------*/
+
+void vApplicationStackOverflowHook( TaskHandle_t pxTask, char *pcTaskName )
+{
+    ( void ) pcTaskName;
+    ( void ) pxTask;
+
+    /* Run time stack overflow checking is performed if
+    configCHECK_FOR_STACK_OVERFLOW is defined to 1 or 2.  This hook
+    function is called if a stack overflow is detected. */
+//    IntMasterDisable();
+    for( ;; );
+}
+/*-----------------------------------------------------------*/
+
+void *malloc( size_t xSize )
+{
+    /* There should not be a heap defined, so trap any attempts to call
+    malloc. */
+//    IntMasterDisable();
+    for( ;; );
+}
+/*-----------------------------------------------------------*/
+
 
